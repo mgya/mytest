ced59104f24443765362c5a3e0696a76c08345e0
wangxiongtao <wangxiongtao@HuYingdeMacBook-Pro.local>
2016-03-01 18:06:03 +0800

    本地消息通知

--- test/test/AppDelegate.m
+++ test/test/AppDelegate.m
@@ -74,4 +74,11 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification*)notification{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"iWeibo" message:notification.alertBody delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+    [alert show];
+    // 图标上的数字减1
+    application.applicationIconBadgeNumber -= 1;
+}
+
 @end
--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -125,6 +125,69 @@
 //    [self jiamijiemi];
 
     
+    // 创建一个本地推送
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    //设置10秒之后
+    NSDate *pushDate = [NSDate dateWithTimeIntervalSinceNow:10];
+    if (notification != nil) {
+        // 设置推送时间
+        notification.fireDate = pushDate;
+        // 设置时区
+        notification.timeZone = [NSTimeZone defaultTimeZone];
+        // 设置重复间隔
+        notification.repeatInterval = kCFCalendarUnitDay;
+        // 推送声音
+        notification.soundName = UILocalNotificationDefaultSoundName;
+        // 推送内容
+        notification.alertBody = @"推送内容";
+        //显示在icon上的红色圈中的数子
+        notification.applicationIconBadgeNumber = 1;
+        //设置userinfo 方便在之后需要撤销的时候使用
+        NSDictionary *info = [NSDictionary dictionaryWithObject:@"name"forKey:@"key"];
+        notification.userInfo = info;
+        //添加推送到UIApplication
+        UIApplication *app = [UIApplication sharedApplication];
+        [app scheduleLocalNotification:notification];   
+    }
+    
+    
+    
+    
+    
+    UIApplication *app = [UIApplication sharedApplication];
+    //获取本地推送数组
+    NSArray *localArray = [app scheduledLocalNotifications];
+    //声明本地通知对象
+    UILocalNotification *localNotification;
+    if (localArray) {
+        for (UILocalNotification *noti in localArray) {
+            NSDictionary *dict = noti.userInfo;
+            if (dict) {
+                NSString *inKey = [dict objectForKey:@"key"];
+                if ([inKey isEqualToString:@"name"]) {
+                    if (localNotification){
+                        localNotification = nil;
+                    }
+                    localNotification = noti;
+                  //  break;
+                }
+            }
+        }
+        
+        //判断是否找到已经存在的相同key的推送
+        if (!localNotification) {
+            //不存在初始化
+            localNotification = [[UILocalNotification alloc] init];
+        }
+        
+        if (localNotification) {
+            //不推送 取消推送
+            [app cancelLocalNotification:localNotification];
+            return;
+        }
+    
+    }
+    
 }
 
 
